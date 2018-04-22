1ea181dc9f7e4a69d63c4ae18faacf62a6d31445
zxmeng <z9meng@uwaterloo.ca>
2018-04-21 21:09:40 -0400

    debug

--- src/main/java/org/wikiclean/DumpEnWikiToPlainText.java
+++ src/main/java/org/wikiclean/DumpEnWikiToPlainText.java
@@ -24,6 +24,8 @@ import org.wikiclean.WikiClean.WikiLanguage;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Simple program for dumping English Wikipedia articles to plain text, one article per line.
@@ -57,19 +59,34 @@ public class DumpEnWikiToPlainText {
     PrintWriter writer = new PrintWriter(args.output, "UTF-8");
     WikipediaArticlesDump wikipedia = new WikipediaArticlesDump(args.input);
 
+    Pattern sentenceRegex = Pattern.compile(
+            "# Match a sentence ending in punctuation or EOS.\n" +
+                    "[\\s]*    # Leading white space\n" + 
+                    "([A-Z\"]    # First char capital letter or quotation\n" +
+                    "[^.!?]*      # Greedily consume up to punctuation.\n" +
+                    "(?:          # Group for unrolling the loop.\n" +
+                    "  [.!?]      # (special) inner punctuation ok if\n" +
+                    "  (?!['\"]?\\s|$)  # not followed by ws or EOS.\n" +
+                    "  [^.!?]*    # Greedily consume up to punctuation.\n" +
+                    ")*           # Zero or more (special normal*)\n" +
+                    "[.!?]?       # Optional ending punctuation.\n" +
+                    "['\"]?)       # Optional closing quote.\n" +
+                    "\\s*$?       # Trailing white space\n",
+                    Pattern.MULTILINE | Pattern.COMMENTS);
+    
     wikipedia.stream()
         .filter(page -> !page.contains("<ns>") || page.contains("<ns>0</ns>"))
         .forEach(page -> {
-          String s = cleaner.clean(page);
+//          String s = cleaner.clean(page);
           
-//          String s = cleaner.clean(page).replaceAll("\\n+", " ");
+          String s = cleaner.clean(page).replaceAll("\\n+", " ");
           if (s.startsWith("#REDIRECT")) return;
           
 //          writer.println(cleaner.getId(page) + ", " + cleaner.getTitle(page).replaceAll("\\n+", " ") + "\t" + s);
 //          writer.println("(" + cleaner.getId(page) + ", " + s + ")");
-          String lines[] = s.split("[\\r\\n]+");
-          for (int i = 0; i < lines.length; i++){
-        	  writer.println(cleaner.getId(page) + "\t" + lines[i]);
+          Matcher m = sentenceRegex.matcher(s);
+          while (m.find()){
+        	  writer.println(cleaner.getId(page) + "\t" + m.group(1));
           }
         });
 
